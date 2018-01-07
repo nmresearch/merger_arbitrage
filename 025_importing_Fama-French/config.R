32df0ef51f9f18b722e4d75873c6a4add285bbec
nmresearch <35150974+nmresearch@users.noreply.github.com>
2018-01-07 11:31:05 -0500

    Create config.R

---
+++ 025_importing_Fama-French/config.R
@@ -0,0 +1,7 @@
+## This file contains the downloaded Fama-French factors.
+file.WRDS.Fama.French.Factors.downloads <-
+  file.path(dir.data, "025_010_WRDS_FAMA_FRENCH_FACTORS.txt")
+
+## This file contains the data.frame 'WRDS.Fama.French.factors'.
+file.WRDS.Fama.French.Factors <-
+  file.path(dir.data, "025_020_WRDS_Fama_French_Factors.RData")
