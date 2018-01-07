de1cc04ce0e7daf78acd8c699f343bb1a1cbe821
nmresearch <35150974+nmresearch@users.noreply.github.com>
2018-01-07 11:43:58 -0500

    Create config.R

---
+++ 120_media_diagram/config.R
@@ -0,0 +1,12 @@
+
+## Time range for media plot. If there are media articles outside of
+## this time range, they will not be included in the plot.
+timerange <- -7:30
+
+## This file contains the complete takeoverData data.frame
+file.SDC.complete <- file.path(dir.data, "090_010_SDC_complete.RData")
+
+## This file contains the data.frame 'factiva'
+file.Factiva <- file.path(dir.data, "100_030_factiva.RData")
+
+
