5e7c51b37e3c1fb1381e09e0a21c5ee81bad3b22
nmresearch <35150974+nmresearch@users.noreply.github.com>
2018-01-07 11:45:33 -0500

    Create config.R

---
+++ 200_panel_data_regression/config.R
@@ -0,0 +1,13 @@
+## Input files
+## This file contains the complete takeoverData data.frame
+file.SDC.complete <- file.path(dir.data, "090_010_SDC_complete.RData")
+
+## This file contains the data.frame 'factiva'
+file.Factiva <- file.path(dir.data, "100_030_factiva.RData")
+
+## This file contains the data.frame 'Tret.00.00'
+file.returns.00.00 <- file.path(dir.data, "107_030_-_020_107_log-returns_00_00.RData")
+
+## Output files
+## This file contains the panel data.frame 'panel'
+file.panel <- file.path(dir.data, "200_010_panel.RData")
