b7a24378916ffb045162e1a33779524f65f21a1d
nmresearch <35150974+nmresearch@users.noreply.github.com>
2018-01-07 11:46:08 -0500

    Create config.R

---
+++ 250_finding_out_fraction_of_deals_in_SP500/config.R
@@ -0,0 +1,11 @@
+## Directory containing the data.
+dir.data <- file.path(dir.base, "Data")
+
+## File containing the takeover data.
+file.sdc <- file.path(dir.data, "095_010_SDC_complete.RData")
+
+## File containing the stock index constituents.
+file.index.constituents <-
+  file.path(dir.data,
+            "250_WRDS_Compustat_Index_Constituents_1998-01_-_2012-03.txt")
+
