4c78f37018824c8a3e5640cec3b68d7355777129
nmresearch <35150974+nmresearch@users.noreply.github.com>
2018-01-07 11:36:01 -0500

    Create config.R

---
+++ 050_consistency_check_for_CUSIP/config.R
@@ -0,0 +1,9 @@
+
+## Directory containing the data.
+dir.data <- file.path(dir.base, "Data")
+
+## This file contains the SDC data with added CUSIP9s.
+file.SDC.with.CUSIP9 <-
+  file.path(dir.data,
+            "035_030_SDC_data_with_CUSIP9.RData")
+
